8ddf54282b415bbeb15f8eb3c653d7446326d85a
Brett Lyons <blyons@students.naropa.edu>
2015-08-03 16:05:22 -0600

    more testing, this.roll function implemented

--- diceRoller.js
+++ diceRoller.js
@@ -1,32 +1,60 @@
-// function diceRoller returns a random # between 1 and 6.
-// snd iteration, diceRoller ( param ) returns param #'s beween 1 and 6
-// third iteration, diceRoller ( param1, param2 ) returns param 1 random numbers between 1 and param2
+// function DiceRoller returns a random # between 1 and 6.
+// snd iteration, DiceRoller ( param ) returns param #'s beween 1 and 6
+// third iteration, DiceRoller ( param1, param2 ) returns param 1 random numbers between 1 and param2
 
 
-//var diceRoller = function ( ) {
+//function DiceRoller1( ) {
   //return (Math.floor((Math.random()*6 + 1 ))) ; // this returns a number
 //}
-//console.log("" + diceRoller());
+//console.log("" + DiceRoller());
 
-//var diceRoller2 = function ( quantDice ) {
+//var DiceRoller2 = function ( quantDice ) {
   //var output = [];
   //for ( var i = 0 ; i < quantDice ; i++ ) {
     //output.push(Math.floor((Math.random()*6 + 1 )))
   //}
 //return output;
 //}
-//console.log(diceRoller2( 3, 10 ) + "");
+//console.log(DiceRoller2( 3, 10 ) + "");
 
-var diceRoller3 = function ( quantDice, numSides ) {
-  var sides = Number(numSides) || 6;
-  console.log("quantDice: " + quantDice + "Sides: " + sides);
-  var output = [];
-  for ( var i = 0 ; i < quantDice ; i++ ) {
-    output.push(Math.floor((Math.random()*sides + 1 )))
+function DiceRoller(numSides) {
+  this.numDice = 1;
+  this.sides = Number(numSides) || 6;
+  this.output = []
+  this.roll = function ( quantDice, localNumSides ) {
+    this.output = [];
+    this.numDice = quantDice || 1;
+    this.sides = localNumSides || this.sides;
+    for ( var i = 0 ; i < this.numDice ; i++ ) {
+      this.output.push(Math.floor((Math.random()*this.sides + 1 )))
+    }
   }
-return output;
+  this.hand = function () {
+    console.log(this.output);
+  }
 }
 
-console.log(diceRoller3( 3 ) + "");
-console.log(diceRoller3( 3, 20 ) + "");
+var game = new DiceRoller();
+game.hand();
+console.log("+++");
+game.roll();
+game.hand();
+console.log("+++");
+game.roll(4);
+game.hand();
+console.log("+++ New Dice Roller");
+var game = new DiceRoller(9);
+game.hand();
+game.roll(5);
+console.log("+++");
+game.hand();
+console.log("Hand Length: " + game.output.length);
+game.roll(4);
+console.log("+++");
+game.hand();
+console.log("Hand Length: " + game.output.length);
+game.roll(7, 10);
+game.hand();
+console.log("Hand Length: " + game.output.length);
+console.log("+++");
 
